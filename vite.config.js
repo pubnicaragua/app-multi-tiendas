4a31e116bb44cf7636f7813b55ffa12590cee642
Benja-Dev <begalan@outlook.es>
2025-03-13 13:19:46 -0600

    Agregando configuración de vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Asegura que las rutas sean correctas en producción
+  server: {
+    historyApiFallback: true, // Maneja las rutas correctamente en Vercel
+  }
 })

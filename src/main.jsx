7c00e0ad5b4b877f91e30c95f05b651d8c453490
Benja-Dev <begalan@outlook.es>
2025-03-14 12:11:58 -0600

    Actualización: Contexto de autenticación

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { NotificationProvider } from './context/NotificationContext' // Importar el provider
+import { AuthProvider } from './context/AuthContext'
 import './index.css'
 import router from '../Router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NotificationProvider>
-      <RouterProvider router={router} />
-    </NotificationProvider>
+    <AuthProvider >
+      <NotificationProvider>
+        <RouterProvider router={router} />
+      </NotificationProvider>
+    </AuthProvider>
   </StrictMode>,
 )

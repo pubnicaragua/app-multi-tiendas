c0f24f2dd629339802074e8525701bc3a6483fc5
Benja-Dev <begalan@outlook.es>
2025-03-14 21:41:56 -0600

    Intentando corregir configuración de vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react(), vercel()],
-  server: {
-    port: import.meta.env.PORT,
-  },
+  plugins: [react()],
 })
 
